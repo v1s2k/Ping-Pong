dff9a62a7fa954b92d7500310248b32ac04c96c7
v1s2k <70753294+v1s2k@users.noreply.github.com>
2021-12-05 16:08:27 +0300

    Add files via upload

---
+++ aksp4/src/main/java/com/example/zdarovamessage/Zdarovamessage.java
@@ -0,0 +1,12 @@
+package com.example.zdarovamessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zdarovamessage {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zdarovamessage.class, args);
+	}
+}
