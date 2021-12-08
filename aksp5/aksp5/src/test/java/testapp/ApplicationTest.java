9cb4cf96f278c6aa3b62ac75d8d3f7b71fea9a3e
v1s2k <70753294+v1s2k@users.noreply.github.com>
2021-12-08 18:22:41 +0300

    AKSP5 VASILEV

--- aksp5/aksp5/src/test/java/testapp/ApplicationTest.java
+++ aksp5/aksp5/src/test/java/testapp/ApplicationTest.java
@@ -2,7 +2,7 @@ package app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test for app
 @SpringBootTest
 class ApplicationTest {
     @Test
